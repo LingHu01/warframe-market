c50546d2aa27ed78f12caa805cfb37cb76e60604
LingHu01 <michel11.zhao@gmail.com>
2023-08-06 22:09:29 +0200

    process data done

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import requests
 import json
 import numpy as np
 
+
 class CheckVitus:
     def __init__(self):
-        self.vitus_dct = None
-        self.item_list = None
-        self.exchange_rate = None
-        self.process()
+        self.all_items = []      # exchange: float:{name: str, list: list(dict), quantity: int}
+        self.quantity = None        # number of item with price below exchange rate
+        self.vitus_dct = None       # {name: vitus cost}
+        self.item_list = None       # one item full API response
+        self.exchange_rate = None   # vitus/plat exchange
+        self.process_data()
 
-    def process(self):  # main
+    # main
+    def process_data(self):
         self.load()
         for name, value in self.vitus_dct.items():
             self.get_data(name)
+            self.refactor()
             self.slice(value)
-            self.calc_exchange(value)
+            self.calc(value)
+            self.all_items.append({self.exchange_rate: {'name': name, 'list': self.item_list, 'quantity': self.quantity}})
             self.item_list = None
 
-    def load(self):  # open js file
+    # open js file
+    def load(self):
         with open('vitus_mod.json', 'r') as fp:
             self.vitus_dct = json.load(fp)
 
@@ -25,12 +32,24 @@ class CheckVitus:
     def filter_dict(dictionary, key, value):
         return True if dictionary[key] == value else False
 
-    def get_data(self, name):  # get item list from wf.market and filter rank and offline player
+    # get item list from wf.market and filter rank and offline player
+    def get_data(self, name):
         item = requests.get(f"https://api.warframe.market/v1/items/{name}/orders")
         lst = [x for x in item.json()["payload"]["orders"] if x["order_type"] == 'sell']
         lst = [x for x in lst if self.filter_dict(x, 'mod_rank', 0)]
         lst = [x for x in lst if self.filter_dict(x['user'], 'status', 'ingame')]
         self.item_list = sorted(lst, key=lambda k: k['platinum'])
+
+    # filter useless info
+    def refactor(self):
+        self.item_list = [
+            {
+                'quantity': dct.get('quantity'),
+                'platinum': dct.get('platinum'),
+                'mod_rank': dct.get('mod_rank'),
+                'seller': dct['user'].get('ingame_name')
+            } for dct in self.item_list
+        ]
 
     def slice(self, value):
         plat, last = (value * 0.75), 0
@@ -39,19 +58,30 @@ class CheckVitus:
                 if dct['platinum'] < plat:
                     return
                 elif dct['platinum'] != last:
-                    self.item_list = self.item_list[:i-1]
+                    self.item_list = self.item_list[:i - 1]
                     return
             last = dct['platinum']
 
-    def calc_exchange(self, value):
+    def calc(self, value):
+        below = 0
         platinum = np.array([dct.get('platinum') for dct in self.item_list if self.filter_dict(dct, 'mod_rank', 0)])
         quantity = np.array([dct.get('quantity') for dct in self.item_list if self.filter_dict(dct, 'mod_rank', 0)])
         average = sum(platinum * quantity) / sum(quantity)
         self.exchange_rate = average / value
+        for price, number in zip(platinum, quantity):
+            if price / value < self.exchange_rate:
+                below += number
+            else:
+                self.quantity = below
+                return
+
+
+
 
 
 def main():
     CheckVitus()
+
 
 if __name__ == '__main__':
     main()
