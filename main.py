289b1b6767a1dadf7673df9c086f849d1b3651e7
LingHu01 <michel11.zhao@gmail.com>
2023-08-06 16:09:46 +0200

    start new branch
    deleted check function

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class CheckVitus:
     def process(self):  # main
         for name, value in self.vitus.items():
             self.get_data(name)
-            self.check(name, value)
 
     def load(self):  # open js file
         with open('vitus_mod.json', 'r') as fp:
@@ -27,24 +26,6 @@ class CheckVitus:
         lst = [x for x in lst if filter_dict(x, 'mod_rank', 0)]
         lst = [x for x in lst if filter_dict(x['user'], 'status', 'ingame')]
         self.sell_item = lst
-
-    def check(self, name, value):
-        plat = value * 0.75
-        sorted_lst = sorted(self.sell_item, key=lambda k: k['platinum'])
-        quantity, peoples = 0, 0
-        for i, x in enumerate(sorted_lst):
-            if x['platinum'] <= plat:
-                peoples += 1
-                quantity += x['quantity']
-            elif x['platinum'] > 15 and i == 5:
-                return self.print_result(name, sorted_lst[:i], quantity, peoples)
-
-    @staticmethod
-    def print_result(name, lst, quantity, people):
-        print(f"{' '.join(name.split('_'))}: {quantity} are sold by {people} people")
-        for x in lst:
-            print(f"quantity: {x['quantity']:4d}, platinum: {x['platinum']:3d}, name: {x['user']['ingame_name']}")
-        print()
 
 def main():
     CheckVitus()
